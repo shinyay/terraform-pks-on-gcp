20f0a863d4f79af6027098ad897228a779d9ae58
shinyay <shinya.com@gmail.com>
2018-08-15 21:22:43 +0900

    Modify mode

--- 3_config-opsman-user.fish
+++ 3_config-opsman-user.fish
@@ -1,8 +1,8 @@
 #!/usr/bin/env fish
 
-set -x OPS_MGR_USR <ACCOUNT NAME for OpsManager>
-set -x OPS_MGR_PWD <ACCOUNT PASSWORD for OpsManager>
-set -x OM_DECRYPTION_PWD <DECRYPTION PASSWORD for OpsManager>
+set -x OPS_MGR_USR admin
+set -x OPS_MGR_PWD admin
+set -x OM_DECRYPTION_PWD admin
 set -x OPSMAN_DOMAIN_OR_IP_ADDRESS (cat ./terraforming-pks-gcp/terraform.tfstate | jq -r '.modules[0].resources."google_compute_address.ops-manager-public-ip".primary.attributes.address')
 om --target https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
                                      --skip-ssl-validation \
