6b84d64a3f4b360c41671eac38ba04eee2fc6883
shinyay <shinya.com@gmail.com>
2018-08-16 14:40:18 +0900

    Remove setting env variable

--- 12_apply-pks.fish
+++ 12_apply-pks.fish
@@ -1,6 +1,5 @@
 #!/usr/bin/env fish
 
-set -x OPSMAN_DOMAIN_OR_IP_ADDRESS (cat ./terraforming-pks-gcp/terraform.tfstate | jq -r '.modules[0].resources."google_compute_address.ops-manager-public-ip".primary.attributes.address')
 om --target "https://$OPSMAN_DOMAIN_OR_IP_ADDRESS" \
    --skip-ssl-validation \
    --username "$OPS_MGR_USR" \
