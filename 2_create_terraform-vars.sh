1ad25538aa2cce7de684accc24964116c5b410b8
shinyay <shinya.com@gmail.com>
2018-08-17 08:44:35 +0900

    Add a shell for creating terraform-var

---
+++ 2_create_terraform-vars.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+cat << EOF > terraform.tfvars
+env_prefix         = "${ACCOUNT_NAME}"
+project          = "${PROJECT_ID}"
+region           = "asia-northeast1"
+zones            = ["asia-northeast1-a", "asia-northeast1-b", "asia-northeast1-c"]
+opsman_image_url = "ops-manager-asia/pcf-gcp-2.2-build.300.tar.gz"
+nat_machine_type = "f1-micro"
+opsman_machine_type = "n1-standard-1"
+service_account_key = <<EOK
+$(cat terraform.key.json)
+EOK
+EOF
+
+mv terraform.tfvars terraforming-pks-gcp
