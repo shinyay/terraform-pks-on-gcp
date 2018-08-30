e137a7b9b9b36bac9590975d94a7326af1d85c9f
shinyay <shinya.com@gmail.com>
2018-08-30 13:13:04 +0900

    Add Pivnet CLI

--- 3_install-om.fish
+++ 3_install-om.fish
@@ -3,3 +3,6 @@
 curl -Lo om https://github.com/pivotal-cf/om/releases/download/0.39.0/om-darwin
 chmod +x om
 mv om /usr/local/bin/
+curl -Lo pivnet https://github.com/pivotal-cf/pivnet-cli/releases/download/v0.0.53/pivnet-linux-amd64-0.0.53
+chmod +x pivnet
+mv pivnet /usr/local/bin/
