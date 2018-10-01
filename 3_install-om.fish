adb8fdbb38436905e1c5014c3792b579e5aca143
shinyay <shinya.com@gmail.com>
2018-10-01 18:26:57 +0900

    Modify OM CLI version as 0.41.0

--- 3_install-om.fish
+++ 3_install-om.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
-curl -Lo om https://github.com/pivotal-cf/om/releases/download/0.39.0/om-darwin
+curl -Lo om https://github.com/pivotal-cf/om/releases/download/0.41.0/om-darwin
 chmod +x om
 mv om /usr/local/bin/
 curl -Lo pivnet https://github.com/pivotal-cf/pivnet-cli/releases/download/v0.0.53/pivnet-darwin-amd64-0.0.53
