b5a918e3b8a430a34c95d398ce635c2420c30726
shinyay <shinya.com@gmail.com>
2018-10-18 19:21:36 +0900

    Add pivnet-cli to download PKS from Pivotal Network

--- 9_upload-pks.fish
+++ 9_upload-pks.fish
@@ -2,17 +2,29 @@
 
 ## PKS Image
 ### https://network.pivotal.io/products/pivotal-container-service/
-set -x FILENAME pivotal-container-service-1.1.4-build.5.pivotal
-set -x DOWNLOAD_URL https://network.pivotal.io/api/v2/products/pivotal-container-service/releases/156571/product_files/190794/download
+set -x FILENAME pivotal-container-service-1.1.6-build.6.pivotal
+set -x DOWNLOAD_URL https://network.pivotal.io/api/v2/products/pivotal-container-service/releases/191110/product_files/221573/download
 
 ## Access Token
 ### https://network.pivotal.io/users/dashboard/edit-profile
-set -x REFRESH_TOKEN 60d176a9c03a48cd8b99cba8738c1711-r
+set -x REFRESH_TOKEN 27f8121e60d946e8b208299277a6d27e-r
 set -x ACCESS_TOKEN (curl -s https://network.pivotal.io/api/v2/authentication/access_tokens -d "{\"refresh_token\":\"$REFRESH_TOKEN\"}" | jq -r .access_token)
 
 ## Download PKS
 set -x PKS_ENV_PREFIX $ACCOUNT_NAME
 set -x ZONE (gcloud compute instances list --filter name:$PKS_ENV_PREFIX-ops-manager | awk 'NR>1 {print $2}')
+
+echo "===== INSTALL START: PIVNET CLI ====="
+
+## Install PIVNET CLI on GCP
+gcloud compute ssh ubuntu@$PKS_ENV_PREFIX-ops-manager \
+    --zone $ZONE \
+    --force-key-file-overwrite \
+    --strict-host-key-checking=no \
+    --quiet \
+    --command "wget -O pivnet https://github.com/pivotal-cf/pivnet-cli/releases/download/v0.0.55/pivnet-linux-amd64-0.0.55 && chmod +x pivnet && sudo mv pivnet /usr/local/bin/"
+
+echo "===== INSTALL END: OM CLI ====="
 
 echo "===== DOWNLOAD START: $FILENAME ====="
 
@@ -21,7 +33,8 @@ gcloud compute ssh ubuntu@$PKS_ENV_PREFIX-ops-manager \
     --force-key-file-overwrite \
     --strict-host-key-checking=no \
     --quiet \
-    --command "wget -O "$FILENAME" --header='Authorization: Bearer $ACCESS_TOKEN' $DOWNLOAD_URL" 
+    --command "pivnet login --api-token=$REFRESH_TOKEN && pivnet accept-eula -p pivotal-container-service -r 1.1.6 && pivnet download-product-files -p pivotal-container-service -r 1.1.6 -i 221573"
+#    --command "wget -O "$FILENAME" --header='Authorization: Bearer $ACCESS_TOKEN' $DOWNLOAD_URL" 
 
 echo "===== DOWNLOAD END: $FILENAME ====="
 
