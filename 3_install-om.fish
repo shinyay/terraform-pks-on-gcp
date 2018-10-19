bb456d5c0564a24c751ff89fb29cf2f208ea5477
shinyay <shinya.com@gmail.com>
2018-10-19 23:43:04 +0900

    Modify pivnet cli version

--- 3_install-om.fish
+++ 3_install-om.fish
@@ -3,6 +3,6 @@
 curl -Lo om https://github.com/pivotal-cf/om/releases/download/0.41.0/om-darwin
 chmod +x om
 mv om /usr/local/bin/
-curl -Lo pivnet https://github.com/pivotal-cf/pivnet-cli/releases/download/v0.0.54/pivnet-darwin-amd64-0.0.53
+curl -Lo pivnet https://github.com/pivotal-cf/pivnet-cli/releases/download/v0.0.54/pivnet-darwin-amd64-0.0.54
 chmod +x pivnet
 mv pivnet /usr/local/bin/
